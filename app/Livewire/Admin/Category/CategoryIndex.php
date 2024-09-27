0d62656bc56fc4b1757f1c4274b589dbf0e2aff9
Sugiarto <sugiarto.dlingo@gmail.com>
2024-09-28 05:48:40 +0700

    feat(admin): CRUD category

--- app/Livewire/Admin/Category/CategoryIndex.php
+++ app/Livewire/Admin/Category/CategoryIndex.php
@@ -2,12 +2,48 @@
 
 namespace App\Livewire\Admin\Category;
 
+use Livewire\Attributes\On;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Modules\Shop\Entities\Category;
 
 class CategoryIndex extends Component
 {
+    public $perPage = 5;
+
+    #[Url(as: 'q')]
+    public ?string $search;
+
+    #[On('category-created')]
+    #[On('category-updated')]
     public function render()
     {
-        return view('livewire.admin.category.category-index');
+        $categories = Category::orderBy('created_at', 'desc');
+
+        if (!empty($this->search)) {
+            $categories = $categories->where('name', 'LIKE', '%'. $this->search . '%');
+        }
+
+        return view('livewire.admin.category.category-index',  [
+            'categories' => $categories->paginate($this->perPage),
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        session()->flash('success', 'Category deleted!');
+    }
+
+    public function changePerPage($perPage)
+    {
+        if (($perPage < 5) || ($perPage > 25)) {
+            $this->perPage = 5;
+            return;
+        }
+
+        $this->perPage = $perPage;
     }
 }
--- app/Livewire/Admin/Category/CreateUpdateCategory.php
+++ app/Livewire/Admin/Category/CreateUpdateCategory.php
@@ -2,12 +2,98 @@
 
 namespace App\Livewire\Admin\Category;
 
+use Illuminate\Validation\Rule;
 use Livewire\Component;
+use Illuminate\Support\Str;
+use Livewire\Attributes\On;
+use Modules\Shop\Entities\Category;
 
 class CreateUpdateCategory extends Component
 {
+    public $editMode = false;
+    public $name;
+    public $category;
+
     public function render()
     {
         return view('livewire.admin.category.create-update-category');
+    }
+
+    protected function rules()
+    {
+        if ($this->editMode == true) {
+            return [
+                'name' => [
+                    'required',
+                    'string',
+                    Rule::unique('shop_categories', 'name')->ignore($this->category->id),
+                ]
+            ];
+        }
+
+        return [
+            'name' => [
+                'required',
+                'string',
+                Rule::unique('shop_categories', 'name'),
+            ]
+        ];
+    }
+
+    public function save()
+    {
+        $validated = $this->validate();
+
+        $params = [
+            'name' => $validated['name'],
+            'slug' => Str::slug($validated['name']),
+        ];
+
+        if (Category::create($params)) {
+            session()->flash('success', 'Category created!');
+
+            $this->dispatch('category-created');
+            $this->reset();
+            return;
+        }
+
+        session()->flash('error', 'Failed');
+    }
+
+    #[On('edit-category')]
+    public function edit($id)
+    {
+        $this->editMode = true;
+
+        $category = Category::findOrFail($id);
+
+        $this->name = $category->name;
+        $this->category = $category;
+    }
+
+    public function update()
+    {
+        $validated = $this->validate();
+    
+        $params = [
+            'name' => $validated['name'],
+            'slug' => Str::slug($validated['name']),
+        ];
+
+        if ($this->category->update($params)) {
+            $this->dispatch('category-updated');
+
+            session()->flash('success', 'Category updated!');
+            $this->reset();
+            return;
+        }
+
+        session()->flash('error', 'Failed');
+    }
+
+
+    public function close()
+    {
+        $this->reset();
     }
 }
